9d4fc2ba239812099633cd140bac87e512686ea5
Carlos Navarrete <cnavarreteliz@gmail.com>
2023-10-23 10:53:48 -0300

    modifies colors in the website

--- src/pages/courses.jsx
+++ src/pages/courses.jsx
@@ -8,7 +8,9 @@ export default function Courses(props) {
       title="Logistics / Supply Chain Management (Spring 2023)"
       subtitle="For Undergraduate students of Industrial Engineering" 
       notes={[
-        {title: "List of Exercises 1", link: "https://docs.google.com/document/d/1RKuDUfosch9SkhJp5l9IIkzGi8pQLdbHSuToZtPFOak/edit?usp=sharing"}
+        {title: "List of Exercises 1", link: "https://docs.google.com/document/d/1RKuDUfosch9SkhJp5l9IIkzGi8pQLdbHSuToZtPFOak/edit?usp=sharing"},
+        {title: "Internship Opportunities", link: "https://docs.google.com/spreadsheets/d/1NCTDLYb4i1caJjlpzrkVuGliYRPvkHJG1iVpWcQulCc/edit?usp=sharing"},
+        {title: "Semester Project", link: "https://docs.google.com/spreadsheets/d/12HEVhhGkL5d8pfGI1rul0x7CjLUZQ-Vkpw-tNbxNbwI/edit?usp=sharing"}
       ]}
     />
   </Page>;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Project from "../components/Project";
-import {SocialMedia} from "../components/SocialMedia";
-import {Title} from "../components/Title";
+import { SocialMedia } from "../components/SocialMedia";
+import { Title } from "../components/Title";
 
-import {papers, projects} from "../helpers/projects";
+import { papers, projects } from "../helpers/projects";
 
 import "./styles.scss";
 
@@ -21,14 +21,14 @@ function IndexPage() {
           <div>
             <p>Let&apos;s have a first call!</p>
             <p>
-             Send an email to <a href="mailto:carlos@navarretelizama.com">carlos@navarretelizama.com</a>
+              Send an email to <a href="mailto:carlos@navarretelizama.com">carlos@navarretelizama.com</a>
             </p></div>
 
           <SocialMedia
             data={[
-              {url: "https://scholar.google.fr/citations?user=JqTxoC0AAAAJ&hl=en", name: "Google Scholar"},
-              {url: "https://github.com/cnavarreteliz", name: "Github"},
-              {url: "https://twitter.com/cnavarreteliz", name: "Twitter"}
+              { url: "https://scholar.google.fr/citations?user=JqTxoC0AAAAJ&hl=en", name: "Google Scholar" },
+              { url: "https://github.com/cnavarreteliz", name: "Github" },
+              { url: "https://twitter.com/cnavarreteliz", name: "Twitter" }
             ]}
           />
 
@@ -44,7 +44,11 @@ function IndexPage() {
           <p>My Ph.D. research was carried out at the <a href="https://irit.fr">Institut de Recherche en Informatique de Toulouse</a> (IRIT) at the Université de Toulouse, at the <a href="https://www.uu.nl/en/research/human-geography-and-planning">Department of Human Geography and Spatial Planning</a> at Utrecht University, and at the <a href="https://uds.udec.cl">Unidad de Data Science</a> at the Universidad de Concepción.</p>
 
           <p>
-          I have participated as a web developer and researcher in several digital projects around the world, such as <a href="https://chilecracia.org">Chilecracia</a>, <a href="https://datachile.io">DataChile</a>, and <a href="https://monprogramme2022.org">Mon Programme 2022</a>, to name a few. Today, my goal is to expand our understanding of society through the use of data science techniques.
+            I have participated as a web developer and researcher in several digital projects around the world, such as <a href="https://chilecracia.org">Chilecracia</a>, <a href="https://datachile.io">DataChile</a>, and <a href="https://monprogramme2022.org">Mon Programme 2022</a>, to name a few.
+          </p>
+
+          <p>
+            Today, my research interests encompass the use and potential applications of Large Language Models (LLMs) and the study of Election Polarization and Competitiveness.
           </p>
 
           {/* <Title label="Recent work" /> */}
