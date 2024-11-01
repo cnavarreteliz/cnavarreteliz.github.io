83bab390317ec40f3fad2ca6d9fb819b72bd5320
Carlos Navarrete <cnavarreteliz@gmail.com>
2024-11-01 18:01:43 -0300

    updates bio

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,7 +20,7 @@ function IndexPage() {
           <div>
             <p>Let&apos;s have a first call!</p>
             <p>
-              Send an email to <a href="mailto:carlos@navarretelizama.com">carlos@navarretelizama.com</a>
+              Send an email to <a href="mailto:cnavarretel@udec.cl">cnavarretel@udec.cl</a>
             </p></div>
 
           <SocialMedia
@@ -35,17 +35,21 @@ function IndexPage() {
         <div className="column">
           <Title label="About me" permalink="about" />
           <p>
-            <strong>Hola!</strong> <b>My name is Carlos Navarrete.</b> I am a Chilean engineer, doctor in computer science, with over eight years of experience developing data-driven platforms worldwide and conducting interdisciplinary research. I am Assistant Professor at the School of Engineering at the Universidad de Concepción, and I am affiliated with the Millennium Nucleus for the Study of Politics, Public Opinion and Media, Chile.
-          </p>
-
-          <p>Prior joining to Concepción, I was a Doctoral Visiting Student at the Department of Human Geography and Spatial Planning at Utrecht University, and at the Centro de Datos e Inteligencia Artificial (CDIA) at the Universidad de Concepción.
-          I completed my doctoral degree at the Université de Toulouse and my undergraduate studies at the Universidad de Concepción.
+            Carlos Navarrete is a Chilean engineer, Ph.D. in computer science, with over eight years of experience developing data-driven platforms worldwide and conducting interdisciplinary research. Carlos serves as an Assistant Professor at the School of Engineering at the Universidad de Concepción, and he is affiliated with the Millennium Nucleus for the Study of Politics, Public Opinion and Media, Chile.
           </p>
 
           <p>
-          My main research sets at the intersection of computer and social science, aiming to unravel the dynamics of society behaviour and polarization. Part of my work has appeared in journals like <a href="https://www.nature.com/articles/s41562-023-01755-x" target="_blank" rel="noreferrer">Nature Human Behaviour</a> and <a href="https://www.sciencedirect.com/science/article/abs/pii/S0048733320300950?via%3Dihub" target="_blank" rel="noreferrer">Research Policy</a>, <a href="https://scholar.google.com/citations?user=JqTxoC0AAAAJ&hl=en" target="_blank" rel="noreferrer">among others</a>.</p>
+          Carlos is one of the leading experts on Data Science and Generative Artificial Intelligence in Chile. He has extensive experience in both leading and collaborating on some of the country&apos;s most widely used data-driven platforms, including Elecciones en La Tercera (<a href="https://data.latercera.com">https://data.latercera.com</a>), DataChile (<a href="https://datachile.io">https://datachile.io</a>), the AI World (<a href="https://aiworld.eu">https://aiworld.eu</a>), and StreamData (<a href="https://streamdata.cl">https://streamdata.cl</a>).
+          </p>
+
+          <p>Prior joining to Concepción, he was a Doctoral Visiting Student at the Department of Human Geography and Spatial Planning at Utrecht University, and at the Centro de Datos e Inteligencia Artificial (CDIA) at the Universidad de Concepción.
+          Carlos completed his doctoral degree at the Université de Toulouse and his undergraduate studies at the Universidad de Concepción.
+          </p>
+
+          <p>
+          His main research sets at the intersection of computer and social science, aiming to unravel the dynamics of society behaviour and polarization. Part of his work has appeared in top journals like <a href="https://www.nature.com/articles/s41562-023-01755-x" target="_blank" rel="noreferrer">Nature Human Behaviour</a> and <a href="https://www.sciencedirect.com/science/article/abs/pii/S0048733320300950?via%3Dihub" target="_blank" rel="noreferrer">Research Policy</a>, <a href="https://scholar.google.com/citations?user=JqTxoC0AAAAJ&hl=en" target="_blank" rel="noreferrer">among others</a>.</p>
           
-          <p>As part of my personal motivations, I am committed to the idea that technology can be used for social good. Currently, I am exploring how Large Language Models can help us in Social Sciences and how digital democracy systems can effectively identify the preferences and concerns of the population.</p>
+          {/* <p>As part of Carlos&apos;s personal motivations, I am committed to the idea that technology can be used for social good. Currently, I am exploring how Large Language Models can help us in Social Sciences and how digital democracy systems can effectively identify the preferences and concerns of the population.</p> */}
           <Title label="Education" permalink="education" />
           <ul>
             <li>B.Sc., Universidad de Concepción (2014)</li>
